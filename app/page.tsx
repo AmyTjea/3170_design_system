0bbbe9fb885b09e9c76ad94dd3f424c3b25db6c6
Amy Tjea <amycaitjea@gmail.com>
2025-08-14 22:53:53 +1000

    feat: added dark mode switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+import { useState } from "react";
 import AlertDisplay from "./component/ui_display/AlertDisplay";
 import ButtonDisplay from "./component/ui_display/ButtonDisplay";
+import { Button } from "./component/ui/button";
 
 export default function Home() {
+
+  const [isDarkMode,setIsDarkMode] = useState(false)
+
+  const toggleTheme = ()=>{
+    setIsDarkMode(!isDarkMode)
+  }
   return (
-    <div className="bg-background p-4 w-full h-full flex flex-col align-middle items-center">
+    <div className={` ${isDarkMode&&"dark "}bg-background p-4 w-full h-full flex flex-col align-middle items-center`}>
+
+      <div><Button onClick={toggleTheme}>Switch theme to {isDarkMode?"Light ":"Dark "}</Button></div>
       <div className="flex flex-col wrap-normal align-middle  ">
         <ButtonDisplay />
         <AlertDisplay/>
